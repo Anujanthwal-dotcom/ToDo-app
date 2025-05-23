c66d4862a0f7079f0912f6a4256174f2fb28966f
Anuj Anthwal <anujanthwal98765432@gmail.com>
2025-05-23 12:49:58 +0530

    routes

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -22,7 +22,6 @@ router.post("/addTask", async (req, res) => {
         const savedList = await list.save();
         dbUser.todo.push(savedList);
         const savedUser = await dbUser.save();
-        console.log(savedUser);
         res.status(200).json({ savedList });
     }
     catch (error) {
@@ -52,6 +51,52 @@ router.put('/updateTask/:id', async (req, res) => {
         res.status(400).json({ message: 'Cannot update task' });
     }
 });
+
+router.delete('/deleteTask/:id', async (req, res) => {
+    try {
+        const { email } = req.body;
+        const dbUser = await User.findOne({email});
+
+        if(!dbUser) {   
+            return res.status(400).json({ message: 'User do not exist' });
+        }
+
+        await List.findByIdAndDelete(req.params.id);
+        dbUser.todo.pull(req.params.id);    
+        await dbUser.save();
+        res.status(200).json({ message: 'Task deleted successfully' });
+    } catch (error) {
+        res.status(400).json({ message: 'Cannot delete task' });
+    }
+});
+
+router.get('/getTasks/:email', async (req, res) => {    
+    try {
+        const dbUser = await User.findOne({email: req.params.email});
+        if(!dbUser) {
+            return res.status(400).json({ message: 'User do not exist' });
+        }
+        const list = dbUser.todo;
+        const lists = await List.find({ _id: { $in: list } });
+        res.status(200).json({ lists });
+    } catch (error) {
+        res.status(400).json({ message: 'Cannot get tasks' });
+    }
+});
+
+router.get('/getTask/:id', async (req, res) => {
+    try {
+        const list = await List.findById(req.params.id);
+        if(!list) {
+            return res.status(400).json({ message: 'Task do not exist' });
+        }
+        res.status(200).json({ list });
+    } catch (error) {
+        res.status(400).json({ message: 'Cannot get task' });
+    }
+});
+
+
 
 
 export default router;
